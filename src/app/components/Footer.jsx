c5d0bf95b85a9aeac9241feb792b12a91032e252
HUSSEN Najjar <154553407+hussenNajjar1@users.noreply.github.com>
2025-04-09 15:07:24 +0300

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -18,8 +18,8 @@ const Footer = () => {
                         <Link href={'http://www.facebook.com/profile.php?id=100085597227744&mibextid=ZbWKwL'} aria-label={'انتقال الى فيسبوك'} ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></Link>
                     </div>
                     <div className='flex  space-x-3'>
-                        <Link href={`https://wa.me/352681520601`} className={`link link-hover ${cairo100.className}` } aria-label={'انتقال الى واتساب'}  > واتساب  </Link>
-                        <Link href={`https://wa.me/352681520601`} aria-label={'انتقال الى واتساب'} ><FaWhatsapp /></Link>
+                        <Link href={`https://wa.me/963941917420`} className={`link link-hover ${cairo100.className}` } aria-label={'انتقال الى واتساب'}  > واتساب  </Link>
+                        <Link href={`https://wa.me/963941917420`} aria-label={'انتقال الى واتساب'} ><FaWhatsapp /></Link>
                     </div>
                     <div className='flex  space-x-3'>
                         <Link href={'https://www.instagram.com/sedra_photography2002?igsh=MTlna3IxYTE0Y2g1Zw=='}  aria-label={'انتقال الى إنستغرام'}  className={`link link-hover ${cairo100.className}`}> إنستغرام  </Link>
